3ae93ecfd5fbcc736440c12599cd55e07995c323
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-05-16 14:20:18 +0700

    chore: setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import {UserRouter} from "./routes";
+
+UserRouter()
--- src/packages/core/infra/mapper.base.ts
+++
@@ -1,7 +0,0 @@
-import { Entity } from "../domain/entitys/entity.base";
-
-export interface Mapper<DomainEntity extends Entity<unknown>, DbRecord, Response = unknown> {
-  toPersistence(entity: DomainEntity): DbRecord;
-  toDomain(record: DbRecord): DomainEntity;
-  toResponse(entity: DomainEntity): Response;
-}
