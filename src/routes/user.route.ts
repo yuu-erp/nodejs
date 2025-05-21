bb494ef6c91a79242b9d816e96c1eac077b1b41c
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-05-21 19:39:46 +0700

    chore: update code main

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,7 +6,6 @@ import { UserService } from '../services/user.service'
 const router = express.Router()
 const prisma = new PrismaService()
 const userService = new UserService(prisma)
-
 const userController = new UserController(userService)
 
 router.get('/', userController.getAllUsers)
