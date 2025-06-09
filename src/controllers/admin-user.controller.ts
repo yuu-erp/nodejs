d47345c24bbc02991957a149ee89c761f852879e
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-06-09 19:57:31 +0700

    fix: fix bug admin api, getUserById

--- src/controllers/admin-user.controller.ts
+++ src/controllers/admin-user.controller.ts
@@ -1,15 +1,12 @@
-import { UserRepository } from '../repositories/user.repository'
 import bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
+import { UserRepository } from '../repositories/user.repository'
 import { isAdmined } from '../utils/isAdmin'
-import { runInThisContext } from 'node:vm'
-
 export class AdminUserController {
-  constructor(private readonly userRepository: UserRepository) { }
+  constructor(private readonly userRepository: UserRepository) {}
 
   createUser = async (req: Request, res: Response) => {
     try {
-
       if (!req.user || !isAdmined(req.user)) {
         res.status(403).json({ message: 'Forbidden' })
         return
@@ -31,13 +28,10 @@ export class AdminUserController {
         return
       }
       const inf = req.body
-      if (!inf) throw new Error("vui lòng điền thông tin")
+      if (!inf) throw new Error('vui lòng điền thông tin')
       const userId = await this.userRepository.findUserById(req.user.id)
-      if (!userId) throw new Error("không tìm thấy user")
-      const updateUser = await this.userRepository.updateUser(
-        userId.id,
-        inf
-      )
+      if (!userId) throw new Error('không tìm thấy user')
+      const updateUser = await this.userRepository.updateUser(userId.id, inf)
       res.status(201).json({ message: 'User updated successfully', updateUser })
     } catch (error) {
       res.status(500).json({ message: 'Internal server error' })
@@ -48,11 +42,11 @@ export class AdminUserController {
   deleteUser = async (req: Request, res: Response) => {
     try {
       const idUser = req.params.id
-      if (!idUser) throw new Error("Cần truyền param id")
+      if (!idUser) throw new Error('Cần truyền param id')
       const user = await this.userRepository.findUserById(idUser)
-      if (!user) throw new Error("không tìm thấy hoặc đã xóa")
+      if (!user) throw new Error('không tìm thấy hoặc đã xóa')
       await this.userRepository.deleteUser(idUser)
-      res.status(200).json({ message: "Xóa người dùng thành công" });
+      res.status(200).json({ message: 'Xóa người dùng thành công' })
       return
     } catch (error) {
       res.status(500).json({ message: 'Internal server error' })
@@ -62,11 +56,19 @@ export class AdminUserController {
 
   getUsers = async (req: Request, res: Response) => {
     await this.userRepository.findAllUsers()
-    return 
+    return
   }
 
   getUserById = async (req: Request, res: Response) => {
-    await this.userRepository.findUserByEmail(req.params.email)
-    return 
+    try {
+      const idUser = req.user?.id
+      if (!idUser) throw new Error('Unauthorized')
+      const user = await this.userRepository.findUserById(idUser)
+      if (!user) throw new Error('không tìm thấy hoặc đã xóa')
+      res.status(200).json({ message: 'Lấy người dùng thành công', data: user })
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' })
+      return
+    }
   }
 }
--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,35 +1,34 @@
-import { Product } from "@prisma/client";
-import { PrismaService } from "../services/prisma.service";
+import { Product } from '@prisma/client'
+import { PrismaService } from '../services/prisma.service'
 
 export class ProductRepostory {
-    constructor(private readonly productPrisma: PrismaService) { }
+  constructor(private readonly productPrisma: PrismaService) {}
 
-    async createItem(item: {
-        name: string;
-        description: string;
-        price: number;
-        stock: number;
-        status: 'AVAILABLE' | 'OUT_OF_STOCK' | 'DISCONTINUED';
-        createdById: string;
-        imageId?: string;
-    }): Promise<Product> {
-        return this.productPrisma.product.create({ data: item });
-    }
-    
-      async findItemById(id: string): Promise<Product | null> {
-        return await this.productPrisma.product.findUnique({ where: { id } })
-      }
-    
-      async updateItem(id: string, item: Product) {
-        return this.productPrisma.product.update({ where: { id }, data: item })
-      }
-    
-      async deleteItem(id: string): Promise<Product> {
-        return await this.productPrisma.product.delete({ where: { id } })
-      }
-    
-      async findAllItem(): Promise<Product[]> {
-        return await this.productPrisma.product.findMany()
-      }
+  async createItem(item: {
+    name: string
+    description: string
+    price: number
+    stock: number
+    status: 'AVAILABLE' | 'OUT_OF_STOCK' | 'DISCONTINUED'
+    createdById: string
+    imageId?: string
+  }): Promise<Product> {
+    return this.productPrisma.product.create({ data: item })
+  }
 
+  async findItemById(id: string): Promise<Product | null> {
+    return await this.productPrisma.product.findUnique({ where: { id } })
+  }
+
+  async updateItem(id: string, item: Product) {
+    return this.productPrisma.product.update({ where: { id }, data: item })
+  }
+
+  async deleteItem(id: string): Promise<Product> {
+    return await this.productPrisma.product.delete({ where: { id } })
+  }
+
+  async findAllItem(): Promise<Product[]> {
+    return await this.productPrisma.product.findMany()
+  }
 }
